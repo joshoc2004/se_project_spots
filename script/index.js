b63a415093102702d2ab618e8b0d7d8a5095042c
joshoc2004 <joshoc2004@gmail.com>
2024-09-19 20:03:58 -0400

    commit clear cd ~

--- script/index.js
+++ script/index.js
@@ -25,8 +25,6 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
-
 const profileEditButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editCloseButton = editProfileModal.querySelector(".modal__close-button");
@@ -35,16 +33,53 @@ const profileName = document.querySelector(".profile__name");
 const editModalNameInput = editProfileModal.querySelector(
   "#profile-name-input"
 );
+const profileDescription = document.querySelector(".profile__description");
+const editModalDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+
+const editFormElement = editProfileModal.querySelector(".modal__form");
+
+const cardTemplate = document.querySelector("#card-template");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardNameElement = cardElement.querySelector(".card__title");
+
+  cardNameElement.textContent = data.name;
+
+  return cardElement;
+}
+
+const cardsList = document.querySelector(".cards__list");
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
 
 function openModal() {
   editModalNameInput.value = profileName.textContent;
+  editModalDescriptionInput.value = profileDescription.textContent;
   editProfileModal.classList.add("modal_opened");
 }
-
-profileEditButton.addEventListener("click", openModal);
 
 function closeModal() {
   editProfileModal.classList.remove("modal_opened");
 }
 
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+  // Update the profile name and description
+  profileName.textContent = editModalNameInput.value;
+  profileDescription.textContent = editModalDescriptionInput.value; // Add this line
+  closeModal(); // Optional: Close the modal after submitting
+}
+
+// Event listeners
+profileEditButton.addEventListener("click", openModal);
 editCloseButton.addEventListener("click", closeModal);
+editFormElement.addEventListener("submit", handleEditFormSubmit);
